9a74ecaa4094c87a6faf38b35afb52302bfc742b
OluwaGB <oluwagbotemioyeboade@gmail.com>
2022-09-16 14:40:03 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+ #include "main.h"
+
+/**
+ *print_alphabet - Entry point
+ *
+ *Description: program that prints the alphabets
+ *followed by a new line
+ *
+ *Return: Exit int function with an int 0
+ */
+void print_alphabet(void)
+{
+	int it;
+
+	for (it = 'a'; it <= 'z'; aj++)
+	{
+		_putchar(it);
+	}
+	_putchar('\n');
+
+}
