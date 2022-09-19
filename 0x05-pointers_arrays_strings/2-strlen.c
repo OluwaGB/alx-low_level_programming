aa2a4fe7e6182e3db8086be522bf97f57d5841d5
OLUWAGBOTEMI <oluwagbotemioyeboade@gmail.com>
2022-09-19 22:46:53 +0100

    function return length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,25 +1,17 @@
 #include "main.h"
 
 /**
-
  * _strlen -  string length
-
  * @s: string
-
  * Return: length as integer;
-
  */
 
 int _strlen(char *s)
-
 {
-
 	int len = 0;
 
 	while (*(s + len) != '\0')
-
 		len++;
 
 	return (len);
-
 }
